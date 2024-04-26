7038b1dd0a64626babc047dbaaa685ed13553949
JonasPHenriksen <jonashenriksenprivat@gmail.com>
2024-04-26 09:55:30 +0200

    test for auto flash

--- src/main.c
+++ src/main.c
@@ -20,7 +20,7 @@ int main() {
   tone_init();
   wifi_init();
 
-  display_setValues(2,8,7,1);
+  display_setValues(1,0,1,2);
 
   wifi_command_join_AP("JOIIIN IOT", "bxww1482");
   tcp_listen("192.168.43.221", 23); // Listen for incoming messages
