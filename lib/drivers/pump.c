fcfe6c8b42ac160382c406f225fd06f0f23c700b
AHowson <Alexander.Howson@gmail.com>
2024-05-01 14:51:34 +0200

    Removed the #inculde for arduino requried libaries and instead did it through our "includes.h" and added a #define for our pc1.

--- lib/drivers/pump.c
+++ lib/drivers/pump.c
@@ -1,7 +1,7 @@
-#include <avr/io.h>
-#include <util/delay.h>
-#include <pump.h>
+#include "pump.h"
+#include "includes.h"
 
+#define PC1 1
 #define OUTPUT_PIN PC1 //PC1 is the pin that we are connected to and using to send the current.
 
 void pump_init() {
