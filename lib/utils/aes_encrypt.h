d57034854eaecc6b8c778299dc7d28b1c78f214a
AHowson <Alexander.Howson@gmail.com>
2024-05-17 12:51:14 +0200

    modified aes_win_aes.c: The test work now but instead of testing the encrypt methods we are testing the calls on them.

--- lib/utils/aes_encrypt.h
+++ lib/utils/aes_encrypt.h
@@ -3,8 +3,10 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h> 
+
+
 #ifndef WINDOWS_TEST
-#include <AESLib.h>
+#include "AESLib.h"
 #endif
 
 uint8_t* encrypt_data(const uint8_t* key, const uint8_t* data, size_t len);
--- test/native/test_win_aes/test_win_aes.c
+++ test/native/test_win_aes/test_win_aes.c
@@ -19,19 +19,32 @@ void setUp(void)
 void tearDown(void) {}
 
 void test_aes_encrypt_encrypt_data_and_decrypt_data() {
-    char* TestString = ("Aes Test");
+    char* TestString = ("{Aes Test}");
     //char* Original_TestString = ("Aes Test");
     uint8_t key[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};  
     uint8_t* encrypted_message = encrypt_data(key,TestString,strlen(TestString));
     uint8_t* decrypted_message = decrypt_data(key,encrypted_message,strlen(encrypted_message));
 
     TEST_ASSERT_EQUAL_STRING(TestString, decrypted_message);
+    TEST_ASSERT_EQUAL(1, aes128_enc_single_fake.call_count);
+    TEST_ASSERT_EQUAL(1, aes128_dec_single_fake.call_count);
 
     // TEST_ASSERT_EQUAL_CHAR_ARRAY_MESSAGE(Original_TestString, TestString, strlen(Original_TestString));
 
 }
 
-void test_aes_encrypt_wrong_key() {
+void test_aes_encrypt_encrypt_data_and_decrypt_long_charPointer() {
+    char* TestString = ("{Aes Testxfh dgj kjnlg hojiæ huiojxj iopd xgdpfhx dfgdxfg hx dhgfhdxg f dxhgfdxfh g xhdji ghdxp hxdfj hpfjohf}");
+    //char* Original_TestString = ("Aes Test");
+    uint8_t key[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    uint8_t* encrypted_message = encrypt_data(key,TestString,strlen(TestString));
+    uint8_t* decrypted_message = decrypt_data(key,encrypted_message,strlen(encrypted_message));
+
+    TEST_ASSERT_EQUAL(7, aes128_enc_single_fake.call_count);
+    TEST_ASSERT_EQUAL(8, aes128_dec_single_fake.call_count);
+    TEST_ASSERT_EQUAL_STRING(TestString, decrypted_message);
+
+    // TEST_ASSERT_EQUAL_CHAR_ARRAY_MESSAGE(Original_TestString, TestString, strlen(Original_TestString));
 
 }
 
@@ -46,6 +59,7 @@ int main(void)
 
     // Run the tests
     RUN_TEST(test_aes_encrypt_encrypt_data_and_decrypt_data);
+    RUN_TEST(test_aes_encrypt_encrypt_data_and_decrypt_long_charPointer);
 
     // End the tests
     return UNITY_END();
