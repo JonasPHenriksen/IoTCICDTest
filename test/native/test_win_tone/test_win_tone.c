8d5c81bcd4380436bb8e7812ed63148625c44167
Twaish <axiomeverything@gmail.com>
2024-05-17 10:14:02 +0200

    Removed duplicate comments. Fixed indentation

--- test/native/test_win_tone/test_win_tone.c
+++ test/native/test_win_tone/test_win_tone.c
@@ -12,79 +12,70 @@ uint8_t DDRA;
 uint8_t CCR2B;
 uint8_t CCR2A;
 
-
 DEFINE_FFF_GLOBALS
 
-
-void setUp(void)
-{
-    FFF_RESET_HISTORY();
+void setUp(void) {
+  FFF_RESET_HISTORY();
 }
 
 void tearDown(void) {}
 
 void test_tone_init(void) {
-    
-    TEST_ASSERT_EQUAL_UINT8(0, DDRA); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, TCCR2A); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, PORTA); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, TCNT2); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, TCCR2B); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, CCR2B); // Ensure measurement is 0 when no echo signal is received4    TEST_ASSERT_EQUAL_UINT8(0, CCR2B); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, CCR2A); // Ensure measurement is 0 when no echo signal is received
+  // Ensure measurement is 0 when no echo signal is received
+  TEST_ASSERT_EQUAL_UINT8(0, DDRA);
+  TEST_ASSERT_EQUAL_UINT8(0, TCCR2A);
+  TEST_ASSERT_EQUAL_UINT8(0, PORTA);
+  TEST_ASSERT_EQUAL_UINT8(0, TCNT2);
+  TEST_ASSERT_EQUAL_UINT8(0, TCCR2B);
+  TEST_ASSERT_EQUAL_UINT8(0, CCR2B);
+  TEST_ASSERT_EQUAL_UINT8(0, CCR2A);
 
-
-
-
-    tone_init();
-
-    TEST_ASSERT_EQUAL_UINT8(128, DDRA); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, TCCR2A); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, PORTA); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, TCNT2); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, TCCR2B); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, CCR2B); // Ensure measurement is 0 when no echo signal is received4    TEST_ASSERT_EQUAL_UINT8(0, CCR2B); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, CCR2A); // Ensure measurement is 0 when no echo signal is received
-
-
+  tone_init();
+  TEST_ASSERT_EQUAL_UINT8(128, DDRA);
+  TEST_ASSERT_EQUAL_UINT8(0, TCCR2A);
+  TEST_ASSERT_EQUAL_UINT8(0, PORTA);
+  TEST_ASSERT_EQUAL_UINT8(0, TCNT2);
+  TEST_ASSERT_EQUAL_UINT8(0, TCCR2B);
+  TEST_ASSERT_EQUAL_UINT8(0, CCR2B);
+  TEST_ASSERT_EQUAL_UINT8(0, CCR2A);
 }
 
 void test_tone_play() {
-        TEST_ASSERT_EQUAL_UINT8(128, DDRA); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, TCCR2A); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, PORTA); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, TCNT2); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, TCCR2B); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, CCR2B); // Ensure measurement is 0 when no echo signal is received4    TEST_ASSERT_EQUAL_UINT8(0, CCR2B); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, CCR2A); // Ensure measurement is 0 when no echo signal is received
-        test_tone_player(392,500);
-        TEST_ASSERT_EQUAL_UINT8(128, DDRA); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, TCCR2A); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, PORTA); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, TCNT2); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(5, TCCR2B); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, CCR2B); // Ensure measurement is 0 when no echo signal is received4    TEST_ASSERT_EQUAL_UINT8(0, CCR2B); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, CCR2A); // Ensure measurement is 0 when no echo signal is received
-    test_tone_player(11,500);
-        TEST_ASSERT_EQUAL_UINT8(128, DDRA); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, TCCR2A); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, PORTA); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, TCNT2); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(7, TCCR2B); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, CCR2B); // Ensure measurement is 0 when no echo signal is received4    TEST_ASSERT_EQUAL_UINT8(0, CCR2B); // Ensure measurement is 0 when no echo signal is received
-    TEST_ASSERT_EQUAL_UINT8(0, CCR2A); // Ensure measurement is 0 when no echo signal is received
+  TEST_ASSERT_EQUAL_UINT8(128, DDRA);
+  TEST_ASSERT_EQUAL_UINT8(0, TCCR2A);
+  TEST_ASSERT_EQUAL_UINT8(0, PORTA);
+  TEST_ASSERT_EQUAL_UINT8(0, TCNT2);
+  TEST_ASSERT_EQUAL_UINT8(0, TCCR2B);
+  TEST_ASSERT_EQUAL_UINT8(0, CCR2B);
+  TEST_ASSERT_EQUAL_UINT8(0, CCR2A);
 
+  test_tone_player(392,500);
+  TEST_ASSERT_EQUAL_UINT8(128, DDRA);
+  TEST_ASSERT_EQUAL_UINT8(0, TCCR2A);
+  TEST_ASSERT_EQUAL_UINT8(0, PORTA);
+  TEST_ASSERT_EQUAL_UINT8(0, TCNT2);
+  TEST_ASSERT_EQUAL_UINT8(5, TCCR2B);
+  TEST_ASSERT_EQUAL_UINT8(0, CCR2B);
+  TEST_ASSERT_EQUAL_UINT8(0, CCR2A);
+
+  test_tone_player(11,500);
+  TEST_ASSERT_EQUAL_UINT8(128, DDRA);
+  TEST_ASSERT_EQUAL_UINT8(0, TCCR2A);
+  TEST_ASSERT_EQUAL_UINT8(0, PORTA);
+  TEST_ASSERT_EQUAL_UINT8(0, TCNT2);
+  TEST_ASSERT_EQUAL_UINT8(7, TCCR2B);
+  TEST_ASSERT_EQUAL_UINT8(0, CCR2B);
+  TEST_ASSERT_EQUAL_UINT8(0, CCR2A);
 }
 
-
 int main(void) {
-    // Initialize the FFF framework
-    UNITY_BEGIN();
-    
-    // Run the tests
-    RUN_TEST(test_tone_init);
-    RUN_TEST(test_tone_play);
+  // Initialize the FFF framework
+  UNITY_BEGIN();
+  
+  // Run the tests
+  RUN_TEST(test_tone_init);
+  RUN_TEST(test_tone_play);
 
-    // End the tests
-    return UNITY_END();
+  // End the tests
+  return UNITY_END();
 }
